06cc68dcedb1d8bdea9bfbbe32fcf979907d3699
dhruv9911 <84926981+dhruv9911@users.noreply.github.com>
2024-07-06 18:21:11 +0530

    Create backend.tf

---
+++ env/sit/backend.tf
@@ -0,0 +1,4 @@
+bucket         = "dev-it-sbxapps-tf-bucket-apse2-01"
+key            = "terraform/dev/eks.tfstate"
+region         = "ap-southeast-2"
+dynamodb_table = "dev-it-sbxapps-backend-terraform-dynamodb-apse2-01"
